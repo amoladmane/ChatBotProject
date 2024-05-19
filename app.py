0e1b6ad276f5564a9740e2e5df2f7ade948ab57f
Amol Admane <155887261+amoladmane@users.noreply.github.com>
2024-05-19 10:18:03 +0000

    gemini

---
+++ app.py
@@ -0,0 +1,33 @@
+# import dependencies
+import gradio as gr
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os 
+
+# load dot env
+load_dotenv()
+genai.configure(api_key=os.getenv("api_key"))
+model = genai.GenerativeModel(model_name="gemini-pro")
+chat = model.start_chat(history=[])
+
+
+# get the model response
+
+def get_model_response (query):
+    response = chat.send_message(query, stream=True)
+    return response
+
+# create gradio interface
+def interface (message , history):
+    response = get_model_response(message)
+    r = []
+    for chunk in response:
+        r.append(chunk.text)
+    return "".join(r)
+
+# create chat GUI
+demo = gr.ChatInterface(fn=interface, title="Gemini Chatbot")
+
+# Run the chatbot
+if __name__ == '__main__':
+    demo.queue().launch()
